8a5c4e53c845a75b4fe8b614a1023615e52f2ffc
Caeden-Mitchell <111471538+Caeden-Mitchell@users.noreply.github.com>
2023-11-08 11:40:33 -0700

    Example of RR 1.0 autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code16544/Automonous/InitialFile.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code16544/Automonous/InitialFile.java
@@ -6,21 +6,46 @@ import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import org.firstinspires.ftc.teamcode.RoadRunner.Drive.MecanumDrive;
 
 @Autonomous
 public class InitialFile extends LinearOpMode {
-    MecanumDrive mecanumDrive = new MecanumDrive(hardwareMap, new Pose2d(0,0,Math.toRadians(90)));
-    @Override
-    public void runOpMode() throws InterruptedException {
-        waitForStart();
+    MecanumDrive mecanumDrive = new MecanumDrive(hardwareMap, new Pose2d(0,0,Math.toRadians(0)));
+    DcMotorEx pixelLift;
 
-        Actions.runBlocking(new SequentialAction(goStraight(10)));
+    public void initialize() {
+        pixelLift = hardwareMap.get(DcMotorEx.class, "pixelLift");
+
+        pixelLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        pixelLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        pixelLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        pixelLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
-    private Action goStraight(double pos) {
-        return mecanumDrive.actionBuilder(mecanumDrive.pose)
-                .lineToY(pos)
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        initialize();
+
+        // This is how trajectories will be built from now on using RR 1.0
+        // This traj will drive forward to a point of 10
+        Action trajAction = mecanumDrive.actionBuilder(mecanumDrive.pose)
+                .lineToX(10)
                 .build();
+
+        waitForStart();
+
+        Actions.runBlocking(new SequentialAction( // sequential action runs actions one after another
+                trajAction, // here it is running the trajectory trajAction
+                telemetryPacket -> { // and then it is lifting the pixel lift
+                    pixelLift.setTargetPosition(600);
+                    pixelLift.setPower(0.6);
+                    sleep(1000);
+                    pixelLift.setPower(0);
+                    return false; // Returning true causes the action to run again, returning false causes it to cease
+                }
+        ));
     }
 }
