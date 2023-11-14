b18b43ec3cb7e114a9ca6a8ae31b7b90801f9cc1
Caeden-Mitchell <111471538+Caeden-Mitchell@users.noreply.github.com>
2023-11-14 12:46:07 -0700

    Auto trajectories

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,27 +1,74 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import com.noahbres.meepmeep.roadrunner.trajectorysequence.TrajectorySequence;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
+
+
+
+       /* TrajectorySequence midDrop = drive.trajectorySequenceBuilder(new Pose2d(-35.57, 35.57, Math.toRadians(0.00)))
+                .splineTo(new Vector2d(8.92, 35.50), Math.toRadians(0))
+                .splineToSplineHeading(new Pose2d(45.0, 35.50, Math.toRadians(180.00)), Math.toRadians(0.00))
+                .build();
+
+        TrajectorySequence rightDrop = drive.trajectorySequenceBuilder(new Pose2d(-35.57, 35.57, Math.toRadians(0.00)))
+                .splineTo(new Vector2d(8.92, 35.50), Math.toRadians(0))
+                .splineToSplineHeading(new Pose2d(45, 43.3, Math.toRadians(180.00)), Math.toRadians(0.00))
+                .build();
+
+        TrajectorySequence leftDrop = drive.trajectorySequenceBuilder(new Pose2d(-35.57, 35.57, Math.toRadians(0.00)))
+                .splineTo(new Vector2d(8.92, 35.50), Math.toRadians(0))
+                .splineToSplineHeading(new Pose2d(45, 29.0, Math.toRadians(180.00)), Math.toRadians(0.00))
+                .build();*/
+
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 10.5)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(-41.67, 65.52, Math.toRadians(270.00)))
+                                .splineToSplineHeading(new Pose2d(-36.5, 35.00, Math.toRadians(180)), Math.toRadians(270.00))
+                                .strafeTo(new Vector2d(-31.41, 35.5))
+
+
+                                .lineToConstantHeading(new Vector2d(8.92, 35.50))
+                                .splineToSplineHeading(new Pose2d(45, 43.3, Math.toRadians(180.00)), Math.toRadians(0.00))
+                                .build()
+                );
+
+        RoadRunnerBotEntity myBot2 = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 10.5)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-41.67, 65.52, Math.toRadians(270.00)))
+                                .splineToSplineHeading(new Pose2d(-31.41, 35.00, Math.toRadians(0.00)), Math.toRadians(270.00))
+                                .turn(Math.toRadians(180))
+                                .strafeTo(new Vector2d(-31.41, 35.5))
+
+
+                                .lineToConstantHeading(new Vector2d(8.92, 35.50))
+                                .splineToSplineHeading(new Pose2d(45, 29.0, Math.toRadians(180.00)), Math.toRadians(0.00))
+                                .build()
+                );
+
+        RoadRunnerBotEntity myBot3 = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 10.5)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-41.67, 65.52, Math.toRadians(270.00)))
+                                .strafeTo(new Vector2d(-36, 34))
+                                .turn(Math.toRadians(-90))
+                                .strafeTo(new Vector2d(-31.41, 35.5))
+
+                                .lineToConstantHeading(new Vector2d(8.92, 35.50))
+                                .splineToSplineHeading(new Pose2d(45, 29.0, Math.toRadians(180.00)), Math.toRadians(0.00))
                                 .build()
                 );
 
@@ -29,6 +76,8 @@ public class MeepMeepTesting {
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
+                .addEntity(myBot2)
+                .addEntity(myBot3)
                 .start();
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code16544/Automonous/Auto1.java
@@ -0,0 +1,80 @@
+package org.firstinspires.ftc.teamcode.Code16544.Automonous;
+
+import android.app.appsearch.SearchResult;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Code16544.RobotSystems.RobotSystems;
+import org.firstinspires.ftc.teamcode.RoadRunner.Drive.MecanumDrive;
+
+@Config
+@Autonomous
+public class Auto1 extends LinearOpMode {
+    MecanumDrive drive;
+    RobotSystems robot;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Pose2d startPose = new Pose2d(-41.67, 65.52, Math.toRadians(270.00));
+
+        drive = new MecanumDrive(hardwareMap, startPose);
+
+        robot = new RobotSystems(hardwareMap);
+
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+
+        Action rightSpike = drive.actionBuilder(startPose)
+                .splineToSplineHeading(new Pose2d(-36.5, 35.00, Math.toRadians(180)), Math.toRadians(270.00))
+                .strafeTo(new Vector2d(-31.41, 35.5))
+                .build();
+
+        Action leftSpike = drive.actionBuilder(startPose)
+                .splineToSplineHeading(new Pose2d(-31.41, 35.00, Math.toRadians(0.00)), Math.toRadians(270.00))
+                .turnTo(Math.toRadians(180))
+                .strafeTo(new Vector2d(-31.41, 35.5))
+                .build();
+
+        Action midSpike = drive.actionBuilder(startPose)
+                .strafeTo(new Vector2d(-36, 34))
+                .turnTo(Math.toRadians(180))
+                .strafeTo(new Vector2d(-31.41, 35.5))
+                .build();
+
+        Action midDrop = drive.actionBuilder(new Pose2d(-31.41, 35.5, Math.toRadians(0.00)))
+                .strafeTo(new Vector2d(45.0, 35.50))
+                .build();
+
+        Action leftDrop = drive.actionBuilder(new Pose2d(-31.41, 35.5, Math.toRadians(0.00)))
+                .strafeToConstantHeading(new Vector2d(8.92, 35.50))
+                .splineToSplineHeading(new Pose2d(45, 43.3, Math.toRadians(180.00)), Math.toRadians(0.00))
+                .build();
+
+        Action rightDrop = drive.actionBuilder(new Pose2d(-31.41, 35.5, Math.toRadians(0.00)))
+                .strafeToConstantHeading(new Vector2d(8.92, 35.50))
+                .splineToSplineHeading(new Pose2d(45, 29.0, Math.toRadians(180.00)), Math.toRadians(0.00))
+                .build();
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        Actions.runBlocking(new SequentialAction(
+                midSpike,
+                telemetryPacket -> {
+                    telemetry.addLine("Action");
+                    return false;
+                },
+                midDrop));
+        
+    }
+}
