3eaaffa8cb4cddcd58cfb886d54445cf8b604223
Nicholas Plumb <starwarsfan12356@gmail.com>
2023-11-17 13:31:10 -0700

    DC

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code16544/DriverControl/DC1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code16544/DriverControl/DC1.java
@@ -16,7 +16,9 @@ public class DC1 extends LinearOpMode {
     RobotSystems robot;
     int isOn = 0;
     int pixelOn = 0;
-    public static double intakePower = 0.065;
+    public static double intakePower = 1;
+
+    private int initialPixelPos = 0;
 
     private enum Height{
         DEAD_STATE,
@@ -25,13 +27,18 @@ public class DC1 extends LinearOpMode {
         HIGH
     }
 
+    public Height height;
 
 
     @Override
     public void runOpMode() throws InterruptedException {
+
         drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
         robot = new RobotSystems(hardwareMap);
-        Height height = Height.DEAD_STATE;
+        height = Height.DEAD_STATE;
+
+        initialPixelPos = robot.pixelLift.getCurrentPosition();
+
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
         double y = 0.0; //left stick y
@@ -49,7 +56,15 @@ public class DC1 extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            if (gamepad1.left_bumper) {
+
+            runGamepad1();
+            runGamepad2();
+
+            telemetry.addData("intake power", robot.intakeMotor.getPower());
+            telemetry.addData("Position", robot.pixelLift.getCurrentPosition());
+            telemetry.update();
+
+            /*if (gamepad1.left_bumper) {
                 //slow mode
                 y = gamepad1.left_stick_y / 7; // Y Stick is reversed
                 x = gamepad1.left_stick_x * 1.1 / 7; //counters imperfect strafing
@@ -59,12 +74,13 @@ public class DC1 extends LinearOpMode {
                 y = gamepad1.left_stick_y; // Y Stick is reversed
                 x = gamepad1.left_stick_x * 1.1; //counters imperfect strafing
                 rx = -gamepad1.right_stick_x;
-            }
+            }*/
 
            //if(!robot.pixelLift.isBusy()) {
                 //set height for pixel lift
-                if (gamepad2.dpad_down) {
+             /*   if (gamepad2.dpad_down) {
                     height = Height.DEAD_STATE;
+
                 }
                 if (gamepad2.dpad_left) {
                     height = Height.LOW;
@@ -95,18 +111,17 @@ public class DC1 extends LinearOpMode {
                 robot.DCLowerHopper();
             if (gamepad2.right_bumper){
                 robot.DCLiftHopper();
-            }
+            }*/
 
-            placePixel();
 
-            if(gamepad1.b){
+
+            /*if(gamepad1.b){
                 robot.intakeMotor.setPower(intakePower);
             } else if(gamepad1.a){
                 robot.intakeMotor.setPower(0);
             } else if (gamepad1.x){
                 robot.intakeMotor.setPower(-intakePower);
-
-            }
+            }*/
 
             //robot.intakeMotor.setPower(gamepad1.right_trigger/6);
             //robot.intakeMotor.setPower(-gamepad1.left_trigger/6);
@@ -117,23 +132,14 @@ public class DC1 extends LinearOpMode {
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio, but only when
             // at least one is out of the range [-1, 1]
-            denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            //denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
             //denominator = 1;
 
-            drive.setDrivePowers(new PoseVelocity2d(new Vector2d(y, x), rx));
+            //drive.setDrivePowers(new PoseVelocity2d(new Vector2d(y, x), rx));
 
            // setDrivePower((y - x + rx) / denominator, (y + x + rx) / denominator, (y + x - rx) / denominator, (y - x - rx) / denominator);
 
 
-            if (gamepad1.right_trigger > 0.5) {
-                robot.liftRobot(500);
-            } else {
-                robot.liftRobot(0);
-            }
-
-            telemetry.addData("intake power", robot.intakeMotor.getPower());
-            telemetry.addData("Position", robot.pixelLift.getCurrentPosition());
-            telemetry.update();
 
         }
     }
@@ -145,7 +151,7 @@ public class DC1 extends LinearOpMode {
         drive.rightBack.setPower(rb);
     }
 
-    private void placePixel(){
+    /*private void placePixel(){
         if(gamepad2.y){
             robot.DCDrop();
         }
@@ -153,5 +159,87 @@ public class DC1 extends LinearOpMode {
             robot.servoToZero();
         if(gamepad2.a)
             robot.deadState();
+    }*/
+
+    private void runGamepad1(){
+        double y = 0.0; //left stick y
+        double x = 0.0; //left stick x
+        double rx = 0.0; //right stick x
+        double denominator = 0.0;
+        //drive train
+        if (gamepad1.left_bumper) {
+            //slow mode
+            y = gamepad1.left_stick_y / 7; // Y Stick is reversed
+            x = gamepad1.left_stick_x * 1.1 / 7; //counters imperfect strafing
+            rx = -gamepad1.right_stick_x / 7;
+        } else {
+            //fast mode
+            y = gamepad1.left_stick_y; // Y Stick is reversed
+            x = gamepad1.left_stick_x * 1.1; //counters imperfect strafing
+            rx = -gamepad1.right_stick_x;
+        }
+        drive.setDrivePowers(new PoseVelocity2d(new Vector2d(y, x), rx));
+
+        //intake
+        if(gamepad1.b){
+            robot.intakeMotor.setPower(intakePower);
+        } else if(gamepad1.a){
+            robot.intakeMotor.setPower(0);
+        } else if (gamepad1.x){
+            robot.intakeMotor.setPower(-intakePower);
+        }
+
+        //robot lift
+        /*
+         * WRITE CODE HERE ONCE WE GET LIFT MECHANISM
+         */
+    }
+
+    private void runGamepad2(){
+        //linear slide
+        if (gamepad2.dpad_down) {
+            height = Height.DEAD_STATE;
+        }
+        if (gamepad2.dpad_left) {
+            height = Height.LOW;
+        }
+        if (gamepad2.dpad_right) {
+            height = Height.MID;
+        }
+        if (gamepad2.dpad_up) {
+            height = Height.HIGH;
+        }
+        switch(height){
+            case DEAD_STATE:
+                robot.setPixelLiftHeight(initialPixelPos);
+                break;
+            case LOW:
+                robot.setPixelLiftHeight(750 + initialPixelPos);
+                break;
+            case MID:
+                robot.setPixelLiftHeight(1500 + initialPixelPos);
+                break;
+            case HIGH:
+                robot.setPixelLiftHeight(3000 + initialPixelPos);
+                break;
+        }
+
+        //hopper
+        if(gamepad2.y) {
+            robot.DCLowerHopper();
+        }
+        if (gamepad2.a){
+            robot.DCLiftHopper();
+        }
+        if (gamepad2.right_bumper){
+            robot.DCDrop();
+        }
+
+        //airplane launcher
+        if(gamepad2.left_bumper){
+            /**
+             *  WRITE ACTION FOR HERE
+             */
+        }
     }
 }
